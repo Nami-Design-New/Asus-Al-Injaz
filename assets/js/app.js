adf2255b04f8c32fca9311cef9a2a79971b17f7a
Ahmedelsayed265 <https://github.com/Ahmedelsayed265>
2023-12-07 20:39:09 +0200

    #4

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,25 +10,6 @@ document.querySelectorAll(".nav-link").forEach(el => {
     menue.classList.remove("show");
   });
 });
-let numbers = document.querySelectorAll(".num");
-let started = false;
-function startCount(element) {
-  let goal = element.dataset.goal;
-  let count = setInterval(() => {
-    element.textContent++;
-    if (element.textContent == goal) {
-      clearInterval(count);
-    }
-  }, 2500 / goal);
-}
-window.onscroll = function () {
-  if (this.scrollY >= document.querySelector(".countries").offsetTop) {
-    if (!started) {
-      numbers.forEach((num) => startCount(num));
-    }
-    started = true;
-  }
-};
 function highlight(el) {
   el.previousElementSibling.classList.add("h");
 }
